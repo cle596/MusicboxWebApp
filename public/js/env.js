8597b9ff100388a87120997d2d478d1448003266
changhan <changhanlee@gmail.com>
2015-11-14 18:52:33 -0500

    env change

--- public/js/env.js
+++ public/js/env.js
@@ -4,13 +4,13 @@ var env = function(size){
     adsr[x] = 1/Math.ceil(size/4) * x;
   }
   for (var x=Math.ceil(size/4);x<Math.ceil(size/4*2);++x){
-    adsr[x] = -.3/Math.ceil(size/4) * x + 1.3;
+    adsr[x] = -.5/Math.ceil(size/4) * x + 1.5;
   }
   for (var x=Math.ceil(size/4*2);x<Math.ceil(size/4*3);++x){
-    adsr[x] = .7;
+    adsr[x] = .5;
   }
   for (var x=Math.ceil(size/4*3);x<size;++x){
-    adsr[x] = -.7/Math.ceil(size/4) * x + 2.8;
+    adsr[x] = -.5/Math.ceil(size/4) * x + 2;
   }
   return adsr;
 }
