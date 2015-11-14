12a78ed1e88fa4a631a97e812ff28719cbfa04a1
changhan <changhanlee@gmail.com>
2015-11-14 11:36:45 -0500

    ok

--- app.js
+++ app.js
@@ -2,4 +2,4 @@ var express = require("express");
 var app = express();
 
 app.use(express.static("public"));
-app.listen(8000);
+app.listen(8002);
--- public/js/main.js
+++ public/js/main.js
@@ -4,6 +4,22 @@ var ctx = new AudioContext();
 var comp = ctx.createDynamicsCompressor();
 comp.threshold.value = -50;
 comp.connect(ctx.destination);
+var gain = {};
+gain.v1 = ctx.createGain();
+gain.v2 = ctx.createGain();
+gain.v3 = ctx.createGain();
+gain.v4 = ctx.createGain();
+gain.v5 = ctx.createGain();
+gain.v1.gain.value = 1;
+gain.v2.gain.value = .1;
+gain.v3.gain.value = .1;
+gain.v4.gain.value = .2;
+gain.v5.gain.value = .2;
+gain.v1.connect(comp);
+gain.v2.connect(comp);
+gain.v3.connect(comp);
+gain.v4.connect(comp);
+gain.v5.connect(comp);
 var env = function(size){
   var adsr = new Float32Array(size);
   for (var x=0;x<Math.ceil(size/4);++x){
@@ -20,7 +36,7 @@ var env = function(size){
   }
   return adsr;
 }
-var make_note = function(nobj){
+var make_note = function(nobj,no){
   var rate = 44100;
   var sample = rate * nobj.duration;
   var src = ctx.createBufferSource();
@@ -28,11 +44,20 @@ var make_note = function(nobj){
   var data = new Float32Array(sample);
   var adsr = env(sample);
   data.forEach(function(y,x,data){
-    data[x] = .2*adsr[x]*Math.sin(2*Math.PI*nobj.pitch/rate*x);
+    data[x] = .2*adsr[x]*
+      (
+        Math.sin(2*Math.PI*nobj.pitch/rate*x)+
+        .1*Math.sin(2*Math.PI*nobj.pitch*2/rate*x)+
+        .1*Math.sin(2*Math.PI*nobj.pitch*3/rate*x)+
+        .1*Math.sin(2*Math.PI*nobj.pitch*4/rate*x)+
+        .1*Math.sin(2*Math.PI*nobj.pitch*5/rate*x)+
+        .1*Math.sin(2*Math.PI*nobj.pitch*6/rate*x)+
+        .1*Math.sin(2*Math.PI*nobj.pitch*7/rate*x)
+      );
   });
   buf.copyToChannel(data,0);
   src.buffer = buf;
-  src.connect(comp);
+  src.connect(gain[no]);
   return src;
 }
 var ntime = {
