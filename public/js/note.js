d74d1e1b85d12943c23a438053f69106583002fd
changhan <changhanlee@gmail.com>
2015-11-14 17:20:21 -0500

    slightly more interesting tone

--- public/js/note.js
+++ public/js/note.js
@@ -8,13 +8,12 @@ var make_note = function(nobj,no){
   data.forEach(function(y,x,data){
     data[x] = .2*adsr[x]*
       (
-        .5*Math.sin(2*Math.PI*nobj.pitch/rate*x)+
-        .01*Math.sin(2*Math.PI*nobj.pitch*2/rate*x)+
-        .01*Math.sin(2*Math.PI*nobj.pitch*3/rate*x)+
-        .01*Math.sin(2*Math.PI*nobj.pitch*4/rate*x)+
-        .01*Math.sin(2*Math.PI*nobj.pitch*5/rate*x)+
-        .01*Math.sin(2*Math.PI*nobj.pitch*6/rate*x)+
-        .01*Math.sin(2*Math.PI*nobj.pitch*7/rate*x)
+        .3*Math.sin(2*Math.PI*nobj.pitch/rate*x)+
+        .05*Math.sin(2*Math.PI*nobj.pitch*2/rate*x)+
+        .4*Math.sin(2*Math.PI*nobj.pitch*3/rate*x)+
+        .05*Math.sin(2*Math.PI*nobj.pitch*4/rate*x)+
+        .05*Math.sin(2*Math.PI*nobj.pitch*5/rate*x)+
+        .05*Math.sin(2*Math.PI*nobj.pitch*6/rate*x)
       );
   });
   buf.copyToChannel(data,0);
