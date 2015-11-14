5be54efe2b100b0a4658e094950a64c27cc894d0
changhan <changhanlee@gmail.com>
2015-11-14 18:49:07 -0500

    ok

--- public/js/main.js
+++ public/js/main.js
@@ -22,7 +22,7 @@ var peek = function(ptime,no){
   }
 }
 var playTimeSet = false;
-var ptime = .5;
+var ptime = 1;
 var timer = new Worker('js/timer.js');
 timer.onmessage = function(e){
   if (!playTimeSet){
