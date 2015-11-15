95df254044a6e469e11c31a131bd303cb6f52f37
changhan <changhanlee@gmail.com>
2015-11-15 11:39:18 -0500

    percussive accompaniment

--- public/js/env.js
+++ public/js/env.js
@@ -1,4 +1,8 @@
 var combine = function() {
+  var sample = 0;
+  for (var x in arguments){
+    sample += arguments[x].length;
+  }
   var arr = new Float32Array(sample);
   var z = 0;
   for (var x in arguments) {
@@ -31,17 +35,17 @@ var voice_env = function(size){
 var envelopes = [voice_env];
 envelopes.push(function(sample) {
   var adsr = new Float32Array(sample);
-  var piece = adsr.slice(0, Math.ceil(sample / 64));
+  var piece = adsr.slice(0, Math.ceil(sample / 16));
   piece.forEach(function(y, x, data) {
-    data[x] = 1 / Math.ceil(sample / 64) * x;
+    data[x] = 1 / Math.ceil(sample / 16) * x;
   });
-  var piece2 = adsr.slice(Math.ceil(sample / 64), Math.ceil(sample / 64*2));
+  var piece2 = adsr.slice(Math.ceil(sample / 16), Math.ceil(sample / 16*2));
   piece2.forEach(function(y, x, data) {
-    data[x] = -.99 / (Math.ceil(sample / 64)) * x + 1;
+    data[x] = -.7 / (Math.ceil(sample / 16)) * x + 1;
   });
-  var piece3 = adsr.slice(Math.ceil(sample / 32 ));
+  var piece3 = adsr.slice(Math.ceil(sample / 8 ));
   piece3.forEach(function(y, x, data) {
-    data[x] = -.01 / (Math.ceil(sample / 32 * 31)) * x + .01;
+    data[x] = -.3 / (Math.ceil(sample / 8 * 7)) * x + .3;
   });
   adsr = combine(piece, piece2, piece3);
   return adsr;
