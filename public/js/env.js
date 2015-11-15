c7f9d6afd800cce97cfe3d132ed143d13b5c2642
changhan <changhanlee@gmail.com>
2015-11-15 11:41:48 -0500

    ok

--- public/js/env.js
+++ public/js/env.js
@@ -17,13 +17,13 @@ var combine = function() {
 
 var voice_env = function(size){
   var adsr = new Float32Array(size);
-  for (var x=0;x<Math.ceil(size/4);++x){
-    adsr[x] = 1/Math.ceil(size/4) * x;
+  for (var x=0;x<Math.ceil(size/8);++x){
+    adsr[x] = 1/Math.ceil(size/8) * x;
   }
-  for (var x=Math.ceil(size/4);x<Math.ceil(size/4*2);++x){
+  for (var x=Math.ceil(size/8);x<Math.ceil(size/4);++x){
     adsr[x] = -.5/Math.ceil(size/4) * x + 1.5;
   }
-  for (var x=Math.ceil(size/4*2);x<Math.ceil(size/4*3);++x){
+  for (var x=Math.ceil(size/4);x<Math.ceil(size/4*3);++x){
     adsr[x] = .5;
   }
   for (var x=Math.ceil(size/4*3);x<size;++x){
