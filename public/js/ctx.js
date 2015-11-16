e47a998a2366d9951400090d8a0635ce277ef4d4
changhan <changhanlee@gmail.com>
2015-11-16 14:21:22 -0500

    convo

--- public/js/ctx.js
+++ public/js/ctx.js
@@ -14,11 +14,6 @@ gain.v2.gain.value = .1;
 gain.v3.gain.value = .1;
 gain.v4.gain.value = .2;
 gain.v5.gain.value = .2;
-gain.v1.connect(convolver);
-gain.v2.connect(convolver);
-gain.v3.connect(convolver);
-gain.v4.connect(convolver);
-gain.v5.connect(convolver);
 var convolver = ctx.createConvolver();
 var soundSource, concertHallBuffer;
 ajaxRequest = new XMLHttpRequest();
@@ -30,6 +25,11 @@ ajaxRequest.onload = function() {
   ctx.decodeAudioData(audioData, function(buffer) {
       concertHallBuffer = buffer;
       convolver.buffer = concertHallBuffer;
+      gain.v1.connect(convolver);
+      gain.v2.connect(convolver);
+      gain.v3.connect(convolver);
+      gain.v4.connect(convolver);
+      gain.v5.connect(convolver);
       convolver.connect(comp);
       comp.connect(ctx.destination);
       ready.innerHTML += "convolver ready <br>";
